e9d64a49431d34b26c443c1ea5abb54f3b6f8de5
vikasojha0536 <58655334+vikasojha0536@users.noreply.github.com>
2023-02-18 13:29:06 +0530

    Create SameTree.java

---
+++ Leetcode2023/February2023/SameTree.java
@@ -0,0 +1,20 @@
+// Time complexity: O(n)
+// Space complexity: O(h) h is the height of the tree
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        
+        if(p == null && q == null) {
+            return true;
+        }
+        if(p == null || q == null) {
+            return false;
+        }
+        if(p.val != q.val ) {
+           return false; 
+        }
+        boolean left = isSameTree(p.left, q.left);
+        boolean right = isSameTree(p.right, q.right);
+        return left && right;
+        
+    }
+}
