79a6dc1b31895fa05a564480c182e32eb65e1da8
vikasojha0536 <58655334+vikasojha0536@users.noreply.github.com>
2023-03-20 00:02:57 +0530

    Create RottingOranges.java

---
+++ Leetcode2023/march2023/RottingOranges.java
@@ -0,0 +1,47 @@
+// TC : O(mxn) + O(4*mxn) = O(4*mxn) where n is the size of row and m is the size of column
+// SC : O(mxn) where n is the size of row and m is the size of column
+class Solution {
+    private static int[][] dirs = {{1,0},{-1,0},{0,1},{0,-1}};
+    public int orangesRotting(int[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length; 
+        int count = 0;
+        int fresh_count =0;
+        Queue<int[]> queue =  new LinkedList<>();
+        // TC O(nxm)
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<m; j++){
+                if(grid[i][j] == 2){
+                   queue.add(new int[]{i,j});
+                }
+                else if(grid[i][j] == 1) {
+                  fresh_count++;
+                }
+            }
+        }
+        
+       while(!queue.isEmpty()) {
+           if(fresh_count == 0) {
+               return count;
+           }
+             int size = queue.size();
+             count++;
+         for(int i=0; i<size; i++) { // O(nxm)
+             int[] point = queue.poll();
+             for(int[] dir: dirs) { // TC : 4*m*n
+                 int x = point[0] + dir[0];
+                 int y = point[1] + dir[1];
+                 if(x < 0 || x>= grid.length || y <0 || y >= grid[x].length || grid[x][y] == 2 || grid[x][y] == 0) {
+                 continue;
+             }
+                 grid[x][y] =2;
+                 queue.add(new int[]{x,y});
+                 fresh_count--;
+             }
+         }
+          
+       }
+       return fresh_count == 0 ? count : -1;
+        
+    }
+}
